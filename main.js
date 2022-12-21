9a5f86d9a2990049b0a467162a643e9b46011214
DeenoDev <deyanoklaasen@outlook.com>
2022-12-21 22:05:10 +0200

    main.js file completed.

---
+++ main.js
@@ -0,0 +1,7 @@
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+      $(".zoom img").css({
+          transform: 'translate3d(-50%, -'+(scroll/100)+'%, 0) scale('+(100 + scroll/5)/100+')',
+          
+      });
+  });
